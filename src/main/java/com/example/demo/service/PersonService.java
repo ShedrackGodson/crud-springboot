bb1ac1f5bde961dcda9efd868f70038d66e6f038
ShedrackGodson <hedrickgodson23@gmail.com>
2020-10-08 23:17:25 +0300

    added PersonService class

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,5 +1,18 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+
 public class PersonService {
+
+    private final PersonDao persondao;
+
+    public PersonService(PersonDao personDao) {
+        this.persondao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return 1;
+    }
 
 }
